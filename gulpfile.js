cdd57844802580aa9a6ba8cfd0f1c49faee67270
want2stayhere <want2stayhere@gmail.com>
2020-09-14 21:26:50 +0300

    blog,blog-in

--- gulpfile.js
+++ gulpfile.js
@@ -12,10 +12,12 @@ var gulp = 			require('gulp');
     autoprefixer =  require('gulp-autoprefixer');
     fileinclude =   require('gulp-file-include');
 	watch = 		require('gulp-watch');
+    wait =          require('gulp-wait');
 
 
 gulp.task('sass', function(){
 	return gulp.src('app/scss/*.scss')
+    .pipe(wait(500))
 	.pipe(sass())
     .pipe(autoprefixer(['last 15 versions']))
 	.pipe(gulp.dest('app/css'))
