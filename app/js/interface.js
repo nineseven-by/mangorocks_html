de22995b202cf707ba1effd6838f724302e51239
Pasha-nineseven <323525Ap>
2020-11-18 18:49:33 +0300

    Дизайн новых блоков для Mango+

--- app/js/interface.js
+++ app/js/interface.js
@@ -2,14 +2,18 @@ $(document).ready(function() {
 	flexibility(document.documentElement);
 	
 
-	if ($('.post__list').length>0) {
-		var base64URL = "data:image/";
-		setTimeout(function(){
-		  	$('.post__list').find( "img" ).each(function( index ) {
-		  		$( this ).attr('src').indexOf(base64URL) == -1 ? $(this).wrap("<a href="+ $( this ).attr('src') +" target='_blank' class='figure__link'></a>") : $(this).wrap("<a href="+ $( this ).data('src') +" target='_blank' class='figure__link'></a>") 
-			});
-		}, 500);
-	}
+	if (window.location.href.indexOf("pets") > -1) {
+		return false
+    } else{
+		if ($('.post__list').length>0) {
+			var base64URL = "data:image/";
+			setTimeout(function(){
+			  	$('.post__list').find( "img" ).each(function( index ) {
+			  		$( this ).attr('src').indexOf(base64URL) == -1 ? $(this).wrap("<a href="+ $( this ).attr('src') +" target='_blank' class='figure__link'></a>") : $(this).wrap("<a href="+ $( this ).data('src') +" target='_blank' class='figure__link'></a>") 
+				});
+			}, 500);
+		}
+    }
 
 
 	if (window.location.href.indexOf("pets") > -1) {
